db90e508c9a2a02f9b6c6774ab071ea38511feab
Piyush1002 <piyusharma166@gmail.com>
2021-02-12 12:18:46 +0530

    add to cart design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
+import HomeContainer from './Containers/HomeContainer'
 
 
 
 
 
-class App extends React.Component{
-  render(){
-    return(
-      <div>
-        <h1>Hello React</h1>
-      </div>
-    )
-  }
+
+function App(){
+  return(
+    <div className="App">
+     <HomeContainer />
+      
+    </div>
+  )
 }
 
 
